57a8c451a8a7ee1f9899857620342067937fd06f
dwesty17 <j.dylan.westerhold@gmail.com>
2019-11-03 00:53:59 -0700

    Profile start time of application

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const GraphqlServer = require("./graphql/server");
 
 const main = async () => {
     console.log("💸 pennyworth is starting!");
+    const startTime = new Date();
 
     const database = new Database();
     await database.init();
 
     const graphqlServer = new GraphqlServer();
     await graphqlServer.start();
+
+    const startUpSeconds = (new Date() - startTime) / 1000;
+    console.log(`💸 pennyworth started in ${startUpSeconds} seconds!`);
 };
 
 (async () => await main())();
